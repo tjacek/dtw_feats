e916a458de9c7441f30d4fed492d9a0141785082
tjacek <tjacek@student.agh.edu.pl>
2018-07-01 10:19:58 -0700

    utils module

---
+++ utils.py
@@ -0,0 +1,11 @@
+import os
+
+def bottom_files(path):
+    all_paths=[]
+    for root, directories, filenames in os.walk(path):
+        if(not directories):
+            paths=[ root+'/'+filename_i 
+                for filename_i in filenames]
+            all_paths+=paths
+    return all_paths
+#print(len(bottom_files('seqs')))
