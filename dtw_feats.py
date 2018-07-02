d887208b775f09099754fb3b9f3ddb77a22fb1e3
tjacek <tjacek@student.agh.edu.pl>
2018-07-02 05:28:48 -0700

    add plot

--- dtw_feats.py
+++ dtw_feats.py
@@ -1,7 +1,10 @@
 import time
-import actions.io,metric,graph,utils
+import actions.io,metric,graph,utils,plot
+import sklearn.manifold
+from sklearn.manifold import TSNE
+from sklearn.manifold import MDS
 
-def compute_pairs(in_path='seqs/skew',out_path='pairs/skew_pairs'):
+def compute_pairs(in_path='seqs/max_z',out_path='pairs/max_z_pairs'):
     read_actions=actions.io.ActionReader()
     print(in_path)
     seqs=read_actions(in_path)
@@ -10,4 +13,16 @@ def compute_pairs(in_path='seqs/skew',out_path='pairs/skew_pairs'):
     print("pairs computadion %d" % (time.time()-t0))
     utils.save_object(pairs.raw_pairs,out_path)
 
-compute_pairs()
+def show_dtw_feats(action_path='seqs/max_z',pairs_path='pairs/max_z_pairs' ):
+    read_actions=actions.io.ActionReader(as_dict=True)
+    seqs=read_actions(action_path)
+    raw_pairs=utils.read_object(pairs_path)
+    dtw_pairs=graph.PairwiseDistance(raw_pairs)
+    action_names=seqs.keys()
+    X=dtw_pairs.as_matrix(action_names)
+    y=[ seqs[name_i].cat for name_i in action_names]
+    embd=TSNE(n_components=2,perplexity=30).fit_transform(X)
+    plot.plot_embedding(embd,y)
+
+#compute_pairs()
+show_dtw_feats()
---
+++ plot.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+from matplotlib import offsetbox
+import numpy as np
+import random
+
+def plot_embedding(X,y,title=None):
+    n_points=X.shape[0]
+    x_min, x_max = np.min(X, 0), np.max(X, 0)
+    X = (X - x_min) / (x_max - x_min)
+    cats=np.unique(y)
+    n_cats= cats.shape[0]
+    random.shuffle(cats)
+    plt.figure()
+    ax = plt.subplot(111)
+    for i in range(n_points):
+        color_i= float(cats[int(y[i])-1]) / float(n_cats)
+        plt.text(X[i, 0], X[i, 1], str(y[i]),
+                   color=plt.cm.Set3( color_i),
+                   fontdict={'weight': 'bold', 'size': 9})
+
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
+    plt.show()
+
+def show_distance(D):
+    plt.imshow(D, zorder=2, cmap='Blues', interpolation='nearest')
+    plt.colorbar();
+    plt.show()
