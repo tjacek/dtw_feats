f5915b2dd2f10e52af765a248b9544e55763275e
tjacek <tjacek@student.agh.edu.pl>
2018-07-02 10:21:33 -0700

    global features

--- dtw_feats.py
+++ dtw_feats.py
@@ -1,19 +1,18 @@
 import time
-import actions.io,metric,graph,utils,plot
-import sklearn.manifold
+import numpy as np
+import actions.io,metric,graph,utils,plot,feats
 from sklearn.manifold import TSNE
-from sklearn.manifold import MDS
 
-def compute_pairs(in_path='seqs/corl',out_path='pairs/corl_pairs'):
+def compute_pairs(in_path='seqs/all',out_path='pairs/all_pairs'):
     read_actions=actions.io.ActionReader()
     print(in_path)
     seqs=read_actions(in_path)
     t0=time.time()
     pairs=graph.make_pairwise_distance(seqs)
-    print("pairs computadion %d" % (time.time()-t0))
+    print("pairs computation %d" % (time.time()-t0))
     utils.save_object(pairs.raw_pairs,out_path)
 
-def show_dtw_feats(action_path='seqs/corl',pairs_path='pairs/corl_pairs',title="corl" ):
+def show_dtw_feats(action_path='seqs/all',pairs_path='pairs/all_pairs',title="all" ):
     read_actions=actions.io.ActionReader(as_dict=True)
     seqs=read_actions(action_path)
     raw_pairs=utils.read_object(pairs_path)
@@ -21,8 +20,21 @@ def show_dtw_feats(action_path='seqs/corl',pairs_path='pairs/corl_pairs',title="
     action_names=seqs.keys()
     X=dtw_pairs.as_matrix(action_names)
     y=[ seqs[name_i].cat for name_i in action_names]
+    tsne_embd(X,y,title)
+
+def show_global_feats(action_path='seqs/all',title="global features"):
+    read_actions=actions.io.ActionReader(as_dict=False)
+    seqs=read_actions(action_path)
+    global_extractor=feats.GlobalFeatures()
+    y=[ action_i.cat for action_i in seqs]
+    X=[ global_extractor(action_i)  for action_i in seqs]
+    X=np.array(X)
+    tsne_embd(X,y,title)
+
+def tsne_embd(X,y,title):
     embd=TSNE(n_components=2,perplexity=30).fit_transform(X)
     plot.plot_embedding(embd,y,title=title)
 
 #compute_pairs()
-show_dtw_feats()
+#show_dtw_feats()
+show_global_feats()
---
+++ feats.py
@@ -0,0 +1,22 @@
+import numpy as np
+import scipy.stats
+
+class GlobalFeatures(object):
+    def __init__(self):
+        self.feature_extractor=[avg,std,skew]
+
+    def __call__(self,action_i):
+        action_array=action_i.as_array()
+        features=[]
+        for extractor_i in self.feature_extractor:
+            features+=list(extractor_i(action_array))
+        return features
+
+def avg(action_array):
+    return np.mean(action_array,axis=0)
+
+def std(action_array):
+    return np.std(action_array,axis=0)
+
+def skew(action_array):
+    return scipy.stats.skew(action_array,axis=0)
