2f1ac093ac4e3bc3482f202d20b7a47ab49ebe5e
tjacek <tjacek@agh.edu.pl>
2018-07-09 17:36:18 +0200

    ensemble learn

--- learn.py
+++ learn.py
@@ -1,9 +1,8 @@
 import numpy as np
 import deep,deep.convnet,deep.train
-#import theano.sandbox.cuda
-#theano.sandbox.cuda.use("gpu")
+import utils
 import theano.gpuarray
-#theano.gpuarray.use("gpu")
+
 def get_dataset(in_path,preproc):
     X,y=deep.read_dataset(in_path)
     y=np.array(y)
@@ -20,4 +19,22 @@ def simple_exp(dataset_path='data/MSR',nn_path='data/nn',compile=False):
     deep.train.train_super_model(X,y,model,num_iter=25)
     model.get_model().save(nn_path)
 
-simple_exp(dataset_path='data/MSR',nn_path='data/nn',compile=False)
+def ensemble_exp(dataset_path='data/MSR',nn_path='data/nn',compile=False):
+    preproc=deep.ImgPreproc(4)
+    X,y,n_cats=get_dataset(dataset_path,preproc)
+    utils.make_dir(nn_path)
+    for j in range(n_cats):
+        nn_path_j=nn_path+'/nn'+str(j)
+        print(nn_path_j)
+        model_path=None if(compile) else nn_path_j        
+        model_j=deep.convnet.get_model(2,preproc,nn_path=model_path)
+        y_j=binarize(y,j)
+        deep.train.train_super_model(X,y_j,model_j,num_iter=150)
+        model_j.get_model().save(nn_path_j)
+
+def binarize(y,cat_j):
+    return [ 0 if(y_i==cat_j) else 1
+               for y_i in y]
+
+#simple_exp(dataset_path='data/MSR',nn_path='data/nn',compile=False)
+ensemble_exp(dataset_path='data/train',nn_path='data/models',compile=False)
