3fdc2e0a3bb5e7ed19a82d71f81f656efe512d6c
tjacek <tjacek@student.agh.edu.pl>
2018-07-11 08:12:35 -0700

    refactore single cls

--- ensemble/single_cls.py
+++ ensemble/single_cls.py
@@ -1,8 +1,12 @@
+import dataset
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 
-def simple_exp(train,test):
+def simple_exp(in_path):
+    full_dataset=dataset.read_dataset(in_path)
+    full_dataset.norm()
+    train,test=full_dataset.split()
     clf=LogisticRegression()
-    clf = clf.fit(train.data, train.target)
-    y_true, y_pred = test.target, clf.predict(test.data)
+    clf = clf.fit(train.X, train.y)
+    y_true, y_pred = test.y, clf.predict(test.X)
     print(classification_report(y_true, y_pred,digits=4))
