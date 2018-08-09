e92b34ff6c5dd2564c6ddd09d33c1ffa6e0343eb
tjacek <tjacek@student.agh.edu.pl>
2018-08-09 08:20:04 -0800

    select_clusters method

--- graph/quality.py
+++ graph/quality.py
@@ -4,8 +4,8 @@ import itertools
 
 class ClusterGraph(graph.Graph):
     def __init__(self, distances,desc):
-        train,test=desc.split()
-        super(ClusterGraph, self).__init__( distances,train)
+#        train,test=desc.split()
+        super(ClusterGraph, self).__init__( distances,desc)
     
     def quality(self):
         all_cats=self.by_cat()
@@ -44,22 +44,27 @@ def best_separation(in_path='mhad/deep_pairs',out_path='mhad/quality.txt'):
         dtw_pairs,insts=graph.read_dtw(path_i)
         dtw_graphs.append(ClusterGraph(dtw_pairs,insts))
     best_cls_ids=best_cls(dtw_graphs)
-    print(best_cls_ids)
-#    names=dtw_graphs[0].keys()
-#    feat_names=[ dtw_graphs[cls_i].get_cat(i) 
-#                    for i,cls_i in enumerate(best_cls_ids)]
-#    feats=[ [dtw_graphs[cls_i].as_vector(name_j,names) 
-#                for name_j in feat_names[i]]
-#                    for i,cls_i in enumerate(best_cls_ids)]
-#    feats=list( itertools.chain.from_iterable(feats))
-#    X=np.array(feats).T
-#    insts=pairs.get_descs(names)
-#    for name_i in :
-#        inst_i.data=X[i]
-#    insts.to_txt(out_path)
+    insts=select_clusters(best_cls_ids,dtw_graphs)
+    insts.to_txt(out_path)
+
+def select_clusters(best_cls_ids,dtw_graphs):
+    full_names=dtw_graphs[0].distances.keys()
+    def get_feature(cls_id,feat_name):
+        return dtw_graphs[cls_id].as_vector(feat_name,names=full_names)
+    
+    feats=[[  get_feature(cls_id,feat_name)
+                for feat_name in dtw_graphs[cls_id].get_cat(cat_id)]
+                    for cat_id,cls_id in enumerate(best_cls_ids)]
+
+    feats=list( itertools.chain.from_iterable(feats))
+    X=np.array(feats).T
+    insts=pairs.get_descs(full_names)
+    for i,name_i in enumerate(full_names):
+        insts[name_i].data=X[i]
+    return insts
 
 def best_cls(dtw_graphs):
     quality=np.array([graph_i.quality()
                         for graph_i in dtw_graphs])
     print(quality)
-    return np.argmax(quality,axis=0)
+    return np.argmin(quality,axis=0)
