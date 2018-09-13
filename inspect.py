ca4f1f04c345c3ad7308b2c40c83fe8069b54abb
tjacek <tjacek@student.agh.edu.pl>
2018-09-13 06:58:20 -0800

    inspect dataset

--- inspect.py
+++ inspect.py
@@ -1,12 +1,11 @@
 import numpy as np
 import seqs,seqs.io,seqs.select 
 
-def show_stats(in_path,img_seq=False):
-    actions=get_actions(in_path,img_seq=img_seq,select=1)
-    n_actions=len(actions)
-    actions_by_cat=seqs.by_cat(actions)
-    cats=[ len(cat_i) for cat_i in actions_by_cat.values()]
-    print(np.median(cats))
+def inspect_dataset(in_path,img_seq=False):
+    actions=get_actions(in_path,img_seq=img_seq,select=None)
+    print(len(actions))
+    show_stats(action_sizes(actions))
+    show_stats(cat_sizes(actions))
 
 def get_actions(in_path,img_seq=False,select=False):
     read_actions=seqs.io.build_action_reader(img_seq=img_seq,as_dict=False)
@@ -15,4 +14,15 @@ def get_actions(in_path,img_seq=False,select=False):
         actions=seqs.select.select(actions,selector=select)
     return actions
 
-show_stats(in_path='mra/seqs/corl',img_seq=False)
+def action_sizes(actions):
+    return [len(action_i) for action_i in actions]
+
+def cat_sizes(actions):
+    actions_by_cat=seqs.by_cat(actions).values()
+    return [ len(cat_i) for cat_i in actions_by_cat]
+
+def show_stats(array):
+    print("median %d" % np.median(array))
+    print("mean %d" % np.mean(array))
+
+inspect_dataset(in_path='mra/seqs/corl',img_seq=False)
