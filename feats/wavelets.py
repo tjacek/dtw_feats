340cb60e5aab5066ed9b8b4683d6718d2eec6797
tjacek <tjacek@student.agh.edu.pl>
2018-09-15 10:25:23 -0800

    wavelets preproc

--- feats/wavelets.py
+++ feats/wavelets.py
@@ -1,13 +1,19 @@
 import numpy as np
 import pywt
 
+class WaveletPreproc(object):
+    def __init__(self,det=0,wavelet_type='db1'):
+        self.det=int(det)
+        self.wavelet_type = wavelet_type
+    
+    def __call__(self,feature_i):
+        print(feature_i)	
+        return pywt.dwt(feature_i,self.wavelet_type)[self.det]
+
 def test_ts(n=25,noise_scale=0.1):
     x=np.arange(n)
     noise=noise_scale*np.random.rand(n)
     return np.sin(x)+noise
 
-result=pywt.dwt(test_ts(), 'db1')
-print(test_ts())
-print(result[0])
-print(result[1])
-	
+preproc=WaveletPreproc()
+print(preproc(test_ts()))
