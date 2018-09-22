8b09f2b3b03322c0f004dfbbd0e646830ab17ce6
tjacek <tjacek@student.agh.edu.pl>
2018-09-22 02:20:32 -0800

    timeless img

--- feats/tools.py
+++ feats/tools.py
@@ -5,15 +5,15 @@ from feats.glob import *
 import scipy.stats
 
 class ActionImgs(object):        
-    def __call__(in_path,out_path):#,local_feats):
-        read_actions=seqs.io.build_action_reader(img_seq=True,as_dict=False)
+    def __call__(self,in_path,out_path):#,local_feats):
+        read_actions=seqs.io.build_action_reader(img_seq=False,as_dict=False)
         actions=read_actions(in_path)
         utils.make_dir(out_path)
-        for action_i in new_actions:
+        for action_i in actions:
             out_i=out_path+'/'+action_i.name+".png"
             cv2.imwrite(out_i,self.get_img(action_i))
 
-class TimeActionImgs(object):
+class TimeActionImgs(ActionImgs):
     def __init__(self,local_feats):
         if(type(local_feats)!=feats.LocalFeatures):
             local_feats=feats.LocalFeatures(local_feats)
@@ -23,9 +23,20 @@ class TimeActionImgs(object):
         dummy_action=action_i(self.local_feats,whole_seq=False) 
         return dummy_action.as_array()
 
-#def timeless_img(action_i):
-#    features=action_i.as_features()
-#    x,y=features[0],features[1]
+class TimelessActionImgs(ActionImgs):
+    def __init__(self,size=(65,65),indexes=(0,1)):
+        self.size=size
+        self.index=indexes
+
+    def get_img(self,action_i):
+        features=action_i.as_features()
+        x,y=features[self.index[0]],features[self.index[1]]
+        time_len=len(x)
+        action_img=np.zeros(self.size)
+        for i in range(time_len):
+            x_i,y_i=int(x[i]),int(y[i]) 
+            action_img[x_i][y_i]=100
+        return action_img
 
 def stats_feats():
     raw_funs=[np.mean,np.std,scipy.stats.skew]
