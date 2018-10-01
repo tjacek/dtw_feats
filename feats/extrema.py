750d0dacc329a6e8c1e1821c074cc4be9d71d6a9
tjacek <tjacek@student.agh.edu.pl>
2018-10-01 12:45:50 -0800

    get_location function

--- feats/extrema.py
+++ feats/extrema.py
@@ -2,10 +2,13 @@ import numpy as np
 
 def count_mins(feature_i):
     extr=local_extr(feature_i)
-    n_min=extr[extr==2].shape[0]
-    n_max=extr[extr==(-2)].shape[0]
+    n_min= get_location(extr,2)[0].shape[0]
+    n_max= get_location(extr,-2)[0].shape[0]
     return [n_min,n_max]
 
 def local_extr(feature_i):
     diff_i=np.diff(feature_i)
     return np.diff( np.sign(diff_i))
+
+def get_location(array_i,value):
+    return np.where(array_i==value)
