1ef9f0d49e187478aa4f08c57af6959c2807f6a1
tjacek <tjacek@student.agh.edu.pl>
2018-10-05 02:59:58 -0800

    split series

--- feats/extrema.py
+++ feats/extrema.py
@@ -10,13 +10,24 @@ def local_extr(feature_i):
     diff_i=np.diff(feature_i)
     return np.diff( np.sign(diff_i))
 
+def split_series(feature_i,indexes):
+    n_pieces=indexes.shape[0]
+    pieces=[feature_i[:indexes[0]]]
+    pieces+=[ feature_i[indexes[i-1]:indexes[i]]
+                for i in range(1,n_pieces)]
+    pieces.append(feature_i[indexes[-1]:])
+    return pieces   
+
 def relative_location(array_i,value):
     pos=np.where(array_i==value)[0][0]
     size=array_i.shape[0]
     return float(pos)/float(size)
 
-def get_window(indexes,feature_i,k):
-    return [ feature_i[(j-k):(j+k)] for j in indexes] 
+def get_window(indexes,feature_i,k=5):
+    def window_helper(j):
+        start=0 if( j-k <0) else (j-k)
+        return feature_i[start:(j+k)]
+    return [ window_helper(j) for j in indexes] 
 
 def get_location(array_i,value):
     return np.where(array_i==value)
