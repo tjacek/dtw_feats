f20e3132bcf2b316a23e4c3a9561a696409f2972
tjacek <tjacek@student.agh.edu.pl>
2018-10-27 03:59:31 -0800

    save as mat

--- inspect.py
+++ inspect.py
@@ -1,5 +1,15 @@
 import numpy as np
+import scipy.io
 import seqs,seqs.io,seqs.select 
+import plot.ts
+import scipy.io
+
+def select_actions(in_path,out_path,cat_i=0,feat_j=0):
+    actions=plot.ts.get_actions(in_path)
+    actions=actions[cat_i+1]
+    feats={ action_k.name:action_k.as_features()[feat_j] 
+                for action_k in actions }
+    scipy.io.savemat(out_path,feats)
 
 def inspect_dataset(in_path,img_seq=False):
     actions=get_actions(in_path,img_seq=img_seq,select=1)
@@ -26,4 +36,5 @@ def show_stats(array):
     print("median %d" % np.median(array))
     print("mean %d" % np.mean(array))
 
-inspect_dataset(in_path='mra/frames/proj',img_seq=True)
+select_actions(in_path='mra/imgs/skew',out_path='../test')
+#inspect_dataset(in_path='mra/frames/proj',img_seq=True)
