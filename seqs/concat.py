8fb06e90d998c42bc99c8a2507f7e16ed19a3bc9
tjacek <tjacek@student.agh.edu.pl>
2018-11-13 02:25:25 +0100

    concat

--- seqs/concat.py
+++ seqs/concat.py
@@ -1,12 +1,18 @@
 import numpy as np
 import seqs,seqs.io 
+import ensemble
+
+def ensemble_concat(base_path='../LSTM/all',n_cats=27):
+    def concat_helper(out_path,in_path):
+        apply_concat(base_path,in_path,out_path)
+    return ensemble.EnsembleFun(in_fun=lambda x:x,out_fun=concat_helper,n_paths=n_cats)
 
 def apply_concat(in_path1='seqs/max_z',in_path2='seqs/all',out_path='seqs/full'):
     read_actions=seqs.io.build_action_reader(img_seq=False,as_dict=True)
     actions1=read_actions(in_path1)
     actions2=read_actions(in_path2)
     unified_actions=concat_actions(actions1,actions2)
-    save_actions=actions.io.ActionWriter(img_seq=False)
+    save_actions=seqs.io.ActionWriter(img_seq=False)
     save_actions(unified_actions,out_path) 
 
 def concat_actions(actions1,actions2,dim=1): 
