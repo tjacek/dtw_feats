a7bd2b6a6c1cd6cf33e7314f226ae1f836f12fd6
tjacek <tjacek@agh.edu.pl>
2021-01-12 20:14:04 +0100

    find_weights

--- diff_evol.py
+++ diff_evol.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.optimize import differential_evolution
-import ens
+import ens,learn
 
 class LossFunction(object):
 	def __init__(self, votes):
@@ -12,15 +12,31 @@ class LossFunction(object):
 		return 1.0-result.get_acc()
 
 def diff_voting(common,deep,clf="LR"):
-	datasets=ens.read_dataset(None,deep)
-	votes=ens.make_votes(common,binary,clf=clf)	
-	loss_fun=LossFunction(votes)
+	datasets=ens.read_dataset(common,deep)
+	weights=find_weights(datasets)
+	results=learn.train_ens(datasets,clf="LR")
+	votes=ens.Votes(results)
+	result=votes.weighted(weights)
+	print(result.get_acc())
+	
+def find_weights(datasets):
+	results=validation_votes(datasets)
+	loss_fun=LossFunction(ens.Votes(results))
 	bound_w = [(0.0, 1.0)  for _ in datasets]
 	result = differential_evolution(loss_fun, bound_w, maxiter=1000, tol=1e-7)
 	weights=result['x']
-	result=votes.weighted(weights)
-	print(result.names)
-	print(result.get_acc())
+	return weights
+
+def validation_votes(datasets,clf="LR"):
+	results=[]
+	for data_i in datasets:
+		data_i.norm()
+		train=data_i.split()[0]
+		clf_i=learn.make_model(train,clf)
+		y_pred=clf_i.predict_proba(train.get_X())
+		result_i =learn.Result(train.get_labels(),y_pred,train.names())
+		results.append(result_i)#print(result_i.get_acc())
+	return results
 
 if __name__ == "__main__":
     dataset="MHAD"
