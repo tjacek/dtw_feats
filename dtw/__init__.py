d8a37eb3c08ccc17ea7f56c0811091fd53d21abd
tjacek <tjacek@agh.edu.pl>
2021-01-16 22:26:44 +0100

    dtw feats

--- dtw/__init__.py
+++ dtw/__init__.py
@@ -1,55 +1,13 @@
-import numpy as np
-import pickle
-from dtaidistance import dtw, dtw_ndim
-import files
+import dtw.pairs,files,feats
 
-class DTWpairs(object):
-	def __init__(self,pairs):
-		self.pairs=pairs
- 	
-	def set(self,key1,key2,data_i):
- 		self.pairs[key1][key2]=data_i
-	
-	def save(self,out_path):
-		with open(out_path, 'wb') as handle:
-			pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
+def compute_feats(pair_path,feat_path):
+	pairs=dtw.pairs.read(pair_path)
+	train=get_train(pairs)
+	dtw_feats=feats.Feats()
+	for name_i in train:
+		dtw_feats[name_i]=pairs.features(name_i,train)
+	dtw_feats.save(feat_path)
 
-def make_dtw_pairs(seqs):
-	pairs={ name_i:{name_i:0.0}
-				for name_i in seqs.keys()}
-	return DTWpairs(pairs)
-
-class Seqs(dict):
-	def __init__(self, arg=[]):
-		super(Seqs, self).__init__(arg)
-
-	def split(self):
-		train,test=files.split(self)
-		return Seqs(train),Seqs(test)
-
-def compute_pairs(in_path,out_path):
-	seqs=read_seqs(in_path)
-	pairs=make_pairwise_distance(seqs)
-	pairs.save(out_path)
-
-def read_seqs(in_path):
-	seqs=Seqs()
-	for path_i in files.top_files(in_path):
-		data_i=np.loadtxt(path_i, delimiter=',')
-		name_i=path_i.split('/')[-1]
-		name_i=files.Name(name_i).clean()#clean(name_i)
-		seqs[name_i]=data_i
-	return seqs
-
-def make_pairwise_distance(seqs):
-	dtw_pairs=make_dtw_pairs(seqs)
-	names=list(seqs.keys())[:10]
-	n_ts=len(names)
-	for i in range(1,n_ts):
-		print(i)
-		for j in range(0,i):
-			name_i,name_j=names[i],names[j]
-			distance_ij=dtw_ndim.distance(seqs[name_i],seqs[name_j])
-			dtw_pairs.set(name_i,name_j,distance_ij)
-			dtw_pairs.set(name_j,name_i,distance_ij)
-	return dtw_pairs
+def get_train(pairs):
+	return [name_i for name_i in pairs.keys()
+				if(files.person_selector(name_i))]
---
+++ dtw/pairs.py
@@ -0,0 +1,66 @@
+import numpy as np
+import pickle
+from dtaidistance import dtw, dtw_ndim
+import files
+
+class DTWpairs(object):
+	def __init__(self,pairs):
+		self.pairs=pairs
+
+	def keys(self):
+		return self.pairs.keys()
+
+	def features(self,name_i,names):
+		return np.array([self.pairs[name_i][name_j]  
+					for name_j in names])
+ 	
+	def set(self,key1,key2,data_i):
+ 		self.pairs[key1][key2]=data_i
+	
+	def save(self,out_path):
+		with open(out_path, 'wb') as handle:
+			pickle.dump(self, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+def read(in_path):
+	with open(in_path, 'rb') as handle:
+		return pickle.load(handle)
+
+def make_dtw_pairs(seqs):
+	pairs={ name_i:{name_i:0.0}
+				for name_i in seqs.keys()}
+	return DTWpairs(pairs)
+
+class Seqs(dict):
+	def __init__(self, arg=[]):
+		super(Seqs, self).__init__(arg)
+
+	def split(self):
+		train,test=files.split(self)
+		return Seqs(train),Seqs(test)
+
+def compute_pairs(in_path,out_path):
+	seqs=read_seqs(in_path)
+	pairs=make_pairwise_distance(seqs)
+	pairs.save(out_path)
+
+def read_seqs(in_path):
+	seqs=Seqs()
+	for path_i in files.top_files(in_path):
+		data_i=np.loadtxt(path_i, delimiter=',')
+		name_i=path_i.split('/')[-1]
+		name_i=files.Name(name_i).clean()#clean(name_i)
+		seqs[name_i]=data_i
+	return seqs
+
+def make_pairwise_distance(seqs):
+	dtw_pairs=make_dtw_pairs(seqs)
+	names=list(seqs.keys())
+	n_ts=len(names)
+	for i in range(1,n_ts):
+		print(i)
+		for j in range(0,i):
+			name_i,name_j=names[i],names[j]
+			distance_ij=dtw_ndim.distance(seqs[name_i],seqs[name_j])
+			dtw_pairs.set(name_i,name_j,distance_ij)
+			dtw_pairs.set(name_j,name_i,distance_ij)
+	return dtw_pairs
