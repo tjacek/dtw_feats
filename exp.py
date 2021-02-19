29458974f7e8cea0fc1413ad7f073aeff54bfe5a
tjacek <tjacek@agh.edu.pl>
2021-02-19 15:55:24 +0100

    simple exp template

--- exp.py
+++ exp.py
@@ -20,62 +20,45 @@ class ExpTemplate(object):
 			fun=ens.ensemble
 		self.fun=fun
 
-	def __call__(self,dtw,deep,binary,out_path,clf="LR"):
-		paths=files.iter_product([[dtw,deep], [binary,None]])
-		paths.append((None,binary))
-		paths.append((dtw+deep,binary))
+	def standard(self,common,binary,out_path=None,clf="LR"):
+		paths=files.iter_product([common,binary])   #list(zip(common,binary))
+		self(paths,out_path,clf)
+
+	def __call__(self,paths,out_path,clf="LR"):
 		lines=[]
 		for common_i,deep_i in paths: 
-			result_i=self.fun(common_i,deep_i,clf=clf)
-			if(result_i):
-				desc=exp_info(common_i,deep_i,result_i)
-				lines.append("%s,%s,%s" % desc)
-		files.to_csv(lines,out_path)
-
-def single_exp(common_path,binary_path,out_path,fun=None,clf="LR"):
-	if(not fun):
-		fun=ens.make_votes
-	if(type(clf)==str):
-		clf=[clf]
-	if(out_path):
-		files.make_dir(out_path)
-	lines=[]
-	for clf_i in clf:
-		desc_i=get_desc(common_path)
-		ens_i=get_desc(binary_path) 
-		out_i="%s/%s,%s,%s" % (out_path,desc_i,ens_i,clf_i)
-		print(out_i)
-		lines.append(out_i)
-		if(not os.path.exists(out_i)):
-			votes=fun(common_path,binary_path,clf=clf_i)
-			votes.save(out_i)
+			if(common_i or deep_i):
+				result_i=self.fun(common_i,deep_i,clf=clf)[0]
+				if(result_i):
+					desc=exp_info(common_i,deep_i,result_i)
+					lines.append("%s,%s,%s" % desc)
+		if(out_path):
+			files.save_txt(lines,out_path)
 		else:
-			votes=learn.read(out_i)
-		print(votes.voting().get_acc())
-	return lines
+			print(lines)
 
-def show_result(in_path,out_path=None,hard=None):
-	lines=[]
-	print("common,ensemble,clf,n_clf,Hard voting,accuracy,precision,recall,fscore")
-	for path_i in files.top_files(in_path):
-		votes_i=learn.read(path_i)
-		if(hard is None):
-			for hard_i in [True,False]:
-				lines.append(show_single(path_i,votes_i,hard_i))
-		else:
-				lines.append(show_single(path_i,votes_i,hard))
-	if(out_path):
-		files.to_csv(lines,out_path)
+#def show_result(in_path,out_path=None,hard=None):
+#	lines=[]
+#	print("common,ensemble,clf,n_clf,Hard voting,accuracy,precision,recall,fscore")
+#	for path_i in files.top_files(in_path):
+#		votes_i=learn.read(path_i)
+#		if(hard is None):
+#			for hard_i in [True,False]:
+#				lines.append(show_single(path_i,votes_i,hard_i))
+#		else:
+#				lines.append(show_single(path_i,votes_i,hard))
+#	if(out_path):
+#		files.to_csv(lines,out_path)
 
-def show_single(path_i,votes_i,binary_i):
-	result_i=votes_i.voting(binary_i)
-	metrics_i=result_i.metrics()
-	metrics_i= [result_i.get_acc()] +list(metrics_i[:3])
-	metrics_i="%.4f,%.4f,%.4f,%.4f" % tuple(metrics_i)
-	prefix_i=path_i.split('/')[-1]#.replace("_",",")
-	line_i="%s,%d,%s,%s" % (prefix_i,len(votes_i),str(binary_i),metrics_i)
-	print(line_i)
-	return line_i
+#def show_single(path_i,votes_i,binary_i):
+#	result_i=votes_i.voting(binary_i)
+#	metrics_i=result_i.metrics()
+#	metrics_i= [result_i.get_acc()] +list(metrics_i[:3])
+#	metrics_i="%.4f,%.4f,%.4f,%.4f" % tuple(metrics_i)
+#	prefix_i=path_i.split('/')[-1]#.replace("_",",")
+#	line_i="%s,%d,%s,%s" % (prefix_i,len(votes_i),str(binary_i),metrics_i)
+#	print(line_i)
+#	return line_i
 
 def exp_info(common_i,binary_i,result_i):
 	desc_common=get_desc(common_i)
@@ -89,10 +72,12 @@ def get_desc(common_path):
 	if(common_path is None):
 		return "-"
 	if(type(common_path)==list):
-		desc=[common_i.split("/")[-2] for common_i in common_path]
+		desc=[get_name(common_i) for common_i in common_path]
 		return "-".join(desc)
-#	raise Exception(common_path)
-	return common_path.split("/")[-2]
+	return get_name(common_path)
+
+def get_name(path_i):
+	return "_".join(path_i.split("/")[-2:])
 
 def find_path(in_path):
 	paths=files.all_files(in_path)
@@ -118,11 +103,14 @@ def find_dtw(in_path,dtw_type="dtw"):
 	return dtw_feats
 
 if __name__ == "__main__":
-	deep=['../ICSS_exp/3DHOI/common/1D_CNN/feats']#,'../ICSS_exp/3DHOI/common/stats/feats']
-	binary='../ICSS_exp/3DHOI/ens/lstm/feats'
-	dtw=['../ICSS_exp/3DHOI/dtw/corl/person', '../ICSS_exp/3DHOI/dtw/max_z/person']
-	dtw_exp=ExpTemplate()
-#	dtw_exp(dtw,deep,binary,"3DHOI.csv")
-#	show_result("3DHOI",hard=False)#,"reduction/SVC.csv")
-	simple_exp=SimpleExp()
-	simple_exp(deep,binary)
+    common1=["../3DHOI_set2/common/feats/max_z/dtw",
+            "../3DHOI_set2/common/feats/corl/dtw",
+            "../3DHOI_set2/common/feats/skew/dtw", 
+            "../3DHOI_set2/common/feats/std/dtw"]
+    common2="../3DHOI_set2/s_dtw"
+    binary1="../3DHOI_set2/binary/1D_CNN"
+    binary2="../3DHOI_set2/binary/stats"
+    common=[None,common1,common2]
+    binary=[None,binary1,binary2]
+    exp=ExpTemplate()
+    exp.standard(common,binary,out_path="test.csv")
