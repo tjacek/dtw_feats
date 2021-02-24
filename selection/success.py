243a6bb99a00d244a977255d04e19d5442c67809
tjacek <tjacek@agh.edu.pl>
2021-02-25 00:11:08 +0100

    acc curve

--- selection/success.py
+++ selection/success.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("..")
 import numpy as np
 import ens
 
@@ -9,3 +11,19 @@ def success_selection(common_path,binary_path,clf="LR"):
     					for cat_i in range(n_cats)]
     acc_matrix=np.array(acc_matrix)
     best=np.argmax(acc_matrix,axis=0)
+    clfs=np.argsort(best)
+    return clfs
+
+def acc_curve(common,binary,clf,clfs):
+	clf_selction=range(2,len(clfs))
+	results=[ens.ensemble(common,binary,False,clf,clfs[:k])[0]
+	 			for k in clf_selction]
+	acc=[result_i.get_acc() for result_i in results]
+	print(acc)
+
+if __name__ == "__main__":
+    dataset="../../dtw_paper/MSR"
+    common="%s/common/MSR_500" % dataset
+    binary="%s/binary/1D_CNN/feats" %dataset
+    clfs=success_selection(common,binary,clf="LR")
+    acc_curve(common,binary,"LR",clfs)
