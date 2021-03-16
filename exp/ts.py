147a8d129274d6a170a85b213b96ff5623aeef4d
tjacek <tjacek@agh.edu.pl>
2021-03-16 17:35:57 +0100

    ts exp

--- exp/ts.py
+++ exp/ts.py
@@ -5,7 +5,7 @@ import files,ens,exp
 def binar_exp(common_path,binary_path,out_path):
 	lines=[]
 	for binary_i in files.top_files(binary_path):
-		binary_i="%s/feats" % binary_i
+#		binary_i="%s/feats" % binary_i
 		result_i=ens.ensemble(common_path,binary_i,
 						clf="LR",binary=False)[0]
 		desc_i=exp.exp_info(common_path,binary_i,result_i)
@@ -14,6 +14,11 @@ def binar_exp(common_path,binary_path,out_path):
 		lines.append(str(desc_i))
 	files.save_txt(lines,out_path)
 
-binary="../../dtw_paper/MSR/binary/1D_CNN_mod"
-common="../../dtw_paper/MSR/common/MSR_500"
-binar_exp(common,binary,"MSR_dtw.csv")
+def ts_exp(common,binary,name):
+	binar_exp(None,binary,"%s.csv" % name)
+	binar_exp(common,binary,"%s_common.csv" % name)
+
+dataset="MSR"
+binary="../../dtw_paper/%s/binary/stats_mod" % dataset
+common="../../dtw_paper/%s/common/MSR_500" % dataset
+ts_exp(common,binary,dataset)
