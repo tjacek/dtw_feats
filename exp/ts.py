55c050cda349eaf5665ed863fb97eb20fe2ba272
tjacek <tjacek@agh.edu.pl>
2021-03-21 01:18:42 +0100

    custom desc

--- exp/ts.py
+++ exp/ts.py
@@ -13,32 +13,34 @@ class Exp(object):
 		for binary_i in files.top_files(binary_path):
 			id_i=binary_i.split('/')[-1]
 			path_i="%s/feats" % binary_i
-			result_i=self.fun(common_path,path_i)
-			desc_i=exp.exp_info(common_path,path_i,result_i)
-			desc_i="%s,%s,%s" % desc_i
+			desc_i=self.fun(common_path,path_i)
 			desc_i="%s,%s,%s" % (info,id_i,desc_i)
 			print(desc_i)
 			lines.append(str(desc_i))
 		return lines
 
-def simple_ens(common_path,path_i):
-	return ens.ensemble(common_path,path_i,
+def simple_ens(common,binary):
+	result_i= ens.ensemble(common,binary,
 						clf="LR",binary=False)[0]
+	desc_i=exp.exp_info(common,binary,result_i)
+	return desc_i[-1]
 
 def select_ens(common,binary):
 	best_set=selection.random_selection(common,binary,1000,20)
-	return ens.ensemble(common,binary, s_clf=best_set,
+	result_i=ens.ensemble(common,binary, s_clf=best_set,
 						clf="LR",binary=False)[0]
+	desc_i=exp.exp_info(common,binary,result_i)
+	return "%d,%s" % (len(best_set),desc_i[-1])
 
 def ts_exp(common,binary,out_path):
 	exp=Exp()
 	lines=exp(None,binary,"A")
-	lines+=exp(common,binary,"E")
+	lines+=exp(common,binary,"F")
 	exp=Exp(select_ens)
 	lines+=exp(common,binary,"H")
 	files.save_txt(lines,out_path)
 
-dataset="MHAD"
+dataset="MSR"
 binary="../../dtw_paper/%s/binary/1D_CNN" % dataset
 common="../../dtw_paper/%s/common/%s_500" % (dataset,dataset)
 ts_exp(common,binary,"%s" % dataset)
