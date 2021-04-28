13111323973d88ad31be0e9e31843c0f1fca27ea
tjacek <tjacek@agh.edu.pl>
2021-04-28 16:27:15 +0200

    dtw voting

--- dtw/voting.py
+++ dtw/voting.py
@@ -1,11 +1,30 @@
 import sys
 sys.path.append("..")
+import numpy as np
+from sklearn.metrics import accuracy_score
 import files,ens,pairs
 
 def basic_exp(in_path):
 	paths=files.get_paths(in_path,name="dtw")
 	result=ens.ensemble(paths[0],None,clf="LR",binary=False)[0]
 	result.report()
+
+def voting(in_path):
+	paths=files.get_paths(in_path,name="pairs")
+	dtw_pairs=read_pairs(paths)
+	pref_dict=get_preferences(dtw_pairs[1])
+	y_pred,y_true=[],[]
+	for name_i,pref_i in pref_dict.items():
+		y_true.append(name_i.get_cat())
+		y_pred.append(border_count(pref_i))
+	print(accuracy_score(y_true,y_pred))
+
+def border_count(pref):
+	n_cats=len(pref)
+	votes=np.zeros((n_cats,))
+	for i,cat_i in enumerate(pref):
+		votes[cat_i]+=(n_cats-i)
+	return np.argmax(votes)
 
 def read_pairs(in_path):
 	if(type(in_path)==list):
@@ -32,6 +51,4 @@ def unique_elem(cats):
 	return unique
 
 in_path="../../ICSS/MHAD/dtw/"#corl/pairs"
-paths=files.get_paths(in_path,name="pairs")
-dtw_pairs=read_pairs(paths)
-print(get_preferences(dtw_pairs[0]))
+voting(in_path)
