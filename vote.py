de3fa7a21c273bfdf8d6be202c9e3fd811ba3a64
tjacek <tjacek@agh.edu.pl>
2021-04-29 21:02:19 +0200

    Preferences class

--- vote.py
+++ vote.py
@@ -3,6 +3,19 @@ from sklearn.metrics import accuracy_score
 from collections import Counter
 import ens,files
 
+class Preferences(object):
+	def __init__(self, order):
+		self.order=np.array(order)
+
+	def n_votes(self):
+		return self.order.shape[0]
+
+	def by_vote(self):
+		return [vote_i for vote_i in self.order]
+
+	def by_order(self):
+		return [ord_i for ord_i in self.order.T]
+
 def ensemble(common_path,binary_path,system=None,clf="LR"):
 	if(system is None):
 		system=borda_count
@@ -25,11 +38,12 @@ def prepare_votes(votes):
 def to_preference(vote_i):
 	pref=[]
 	for vote_j in vote_i: 
-		ord_i= np.argsort(vote_j)#np.flip(np.argsort(vote_j))
+		ord_i= np.argsort(vote_j)
 		pref.append(ord_i)
-	return pref
+	return Preferences(pref)
 
 def borda_count(prefer):
+	raise Exception( prefer.by_order())
 	votes=np.zeros( (len(prefer),))
 	for prefer_i in prefer:
 		for j,cat_j in enumerate(prefer_i):
@@ -48,11 +62,26 @@ def bucklin(prefer):
 				return np.argmax(votes)
 	raise Exception("OK")
 
-def k_aproval(prefer,k=5):
+def k_aproval(prefer,k=1):
 	prefer=np.array(prefer)
 	aprov=prefer[:,-k:].flatten()
 	votes=Counter(aprov)
 	return votes.most_common()[0][0]
+
+def coombs(prefer):
+	n_cats=prefer[0].shape[0]
+	major=n_cats/2
+	votes=[Counter(prefer_i) 
+		for prefer_i in np.array(prefer).T]
+
+	votes.reverse()
+	vote_i=votes[0]
+	cat_i=vote_i.most_common()[0]
+	count_i=cat_i[1]
+	if(count_i>major):
+		print(cat_i)
+		return cat_i[0]
+	raise Exception(count_i)
 
 dataset="3DHOI"
 dir_path="../ICSS/%s" % dataset
@@ -60,4 +89,4 @@ common="%s/dtw" % dir_path
 common=files.get_paths(common,name="dtw")
 common.append("%s/1D_CNN/feats" % dir_path)
 binary="%s/ens/feats" % dir_path 
-ensemble(common,binary,system=k_aproval,clf="LR")
+ensemble(common,binary,system=None,clf="LR")
