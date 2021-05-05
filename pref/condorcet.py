7012be383c40b5bc4084ed206f3b7fdc84523b6c
tjacek <tjacek@agh.edu.pl>
2021-05-05 23:43:24 +0200

    full stats

--- pref/condorcet.py
+++ pref/condorcet.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append("..")
+import numpy as np
 from ens import Votes
+from sklearn.metrics import accuracy_score
 import pref
 
 def count_condor(in_path):
@@ -10,9 +12,14 @@ def count_condor(in_path):
 	votes=pref.prepare_votes(votes)
 	votes=[ pref.to_preference(vote_i) 
 			for vote_i in votes]
-	condor=[is_condorcet_winner(win_i,votes[i]) 
+	condor=[1-int(is_condorcet_winner(win_i,votes[i])) 
 				for i,win_i in enumerate(y_pred)]
-	print(condor)
+	print(accuracy_score(y_true,y_pred))
+	print(np.mean(condor))
+	print(np.sum(condor))
+	error=error_cond(y_true,y_pred,condor)
+	print(np.mean(error))
+	print(np.sum(error))
 
 def is_condorcet_winner(win_i,pref_i):
 	n_cats=pref_i.order.shape[0]
@@ -22,6 +29,14 @@ def is_condorcet_winner(win_i,pref_i):
 				return False
 	return True
 
+def error_cond(y_true,y_pred,condor):
+	error=[]
+	for i,cond_i in enumerate(condor):
+		if(y_true[i]!=y_pred[i]):
+			error.append(cond_i)
+	return error
 
-dataset="3DHOI"
+
+
+dataset="MSR"
 count_condor("../s_SVC/%s" % dataset)
