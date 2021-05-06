9977612b39b2d9b7c6346b5d6c55aebe92431f89
tjacek <tjacek@agh.edu.pl>
2021-05-06 17:37:26 +0200

    meta ensemble

--- pref/condorcet.py
+++ pref/condorcet.py
@@ -5,21 +5,40 @@ from ens import Votes
 from sklearn.metrics import accuracy_score
 import pref
 
+def meta_ensemble(main_path,minor_path):
+	y_true,main_pred,main_condor=get_condor(main_path)
+	y_true,minor_pred,minor_condor=get_condor(minor_path)
+	y_meta=[]
+	for i,main_i in enumerate(main_pred):
+		if((not main_condor[i]) and (minor_condor[i])):
+			print(y_true[i])
+			print(minor_pred[i])
+			print("&&&&")
+			y_meta.append(minor_pred[i])
+		else:
+			y_meta.append(main_i)
+	print(accuracy_score(y_true,y_meta))
+
 def count_condor(in_path):
+	y_true,y_pred,condor=get_condor(in_path)
+	print(accuracy_score(y_true,y_pred))
+	print(np.mean(condor))
+	print(len(condor)-np.sum(condor))
+	error=error_cond(y_true,y_pred,condor)
+	print(np.mean(error))
+	print(len(error)-np.sum(error))
+
+def get_condor(in_path):
 	votes=pref.read_pref(in_path)
 	result=votes.voting(False)
 	y_true,y_pred=result.as_labels()
 	votes=pref.prepare_votes(votes)
 	votes=[ pref.to_preference(vote_i) 
 			for vote_i in votes]
-	condor=[1-int(is_condorcet_winner(win_i,votes[i])) 
-				for i,win_i in enumerate(y_pred)]
-	print(accuracy_score(y_true,y_pred))
-	print(np.mean(condor))
-	print(np.sum(condor))
-	error=error_cond(y_true,y_pred,condor)
-	print(np.mean(error))
-	print(np.sum(error))
+	condor=np.array([is_condorcet_winner(win_i,votes[i]) 
+						for i,win_i in enumerate(y_pred)])
+	condor=condor.astype(int)
+	return y_true,y_pred,condor
 
 def is_condorcet_winner(win_i,pref_i):
 	n_cats=pref_i.order.shape[0]
@@ -36,7 +55,6 @@ def error_cond(y_true,y_pred,condor):
 			error.append(cond_i)
 	return error
 
-
-
-dataset="MSR"
-count_condor("../s_SVC/%s" % dataset)
+dataset="3DHOI"
+#count_condor("../s_LR/%s" % dataset)
+meta_ensemble("../s_LR/%s" % dataset,"../s_SVC/%s" % dataset)
--- pref/desc.py
+++ pref/desc.py
@@ -3,7 +3,7 @@ sys.path.append("..")
 from ens import Votes
 import exp,systems,pref,files
 
-def exp_desc(in_path,out_path,info="SVC"):
+def exp_desc(in_path,out_path=None,info="SVC"):
 	votes=pref.read_pref(in_path)
 	all_systems=[systems.borda_count,
 				systems.bucklin,
@@ -14,12 +14,15 @@ def exp_desc(in_path,out_path,info="SVC"):
 		name_i=system_i.__name__
 		result_i=pref.voting(votes,system_i)
 		lines.append(get_line(name_i,result_i,info))
-	files.save_txt(lines,out_path)
+	if(out_path):
+		files.save_txt(lines,out_path)
+	else:
+		print(lines)
 
 def get_line(name_i,result_i,info):
 	metrics_i=exp.get_metrics(result_i)
 	return "%s,%s,%s" % (name_i,info,metrics_i)
 
 if __name__ == "__main__":
-	dataset="MSR"
-	exp_desc("../SVC/%s" % dataset,dataset)
+	dataset="3DHOI"
+	exp_desc("../s_SVC/%s" % dataset,dataset)
