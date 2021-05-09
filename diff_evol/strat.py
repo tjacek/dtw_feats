b60c05b7c89fc7b59e4e2333a06c710c079ba34a
tjacek <tjacek@agh.edu.pl>
2021-05-09 19:11:08 +0200

    find weights

--- diff_evol/strat.py
+++ diff_evol/strat.py
@@ -1,17 +1,39 @@
 import sys
 sys.path.append("..")
 #from sklearn.model_selection import StratifiedShuffleSplit
+import numpy as np
+from sklearn.metrics import log_loss
+from scipy.optimize import minimize
 import random
 import ens,learn,exp,files
 
-def diff_voting(common,deep,clf="LR",n_split=10):
+def split_voting(common,deep,clf="LR"):#,n_split=10):
 	datasets=ens.read_dataset(common,deep)
+	weights=find_weights(datasets)
+	votes=ens.Votes([learn.train_model(data_i,clf_type=clf)
+				for data_i in datasets])
+	result=votes.weighted(weights)
+	result.report()
+
+def find_weights(datasets):
 	datasets=[data_i.split()[0] for data_i in datasets]
 	names=list(datasets[0].keys())
-	votes=[shuffle_split(names,datasets,test_size=0.25)
-			for i in range(n_split)]
-	acc=[ vote_i.voting(False).get_acc() for vote_i in votes]
-	print(acc)
+#	votes=[shuffle_split(names,datasets,test_size=0.25)
+#			for i in range(n_split)]
+	votes=shuffle_split(names,datasets,test_size=0.25)
+	def log_loss_func(weights):
+		result=votes.weighted(weights)
+		y_true=to_one_hot(result.y_true,len(votes))
+		y_pred=result.y_pred
+		return log_loss(y_true,y_pred)
+	starting_values = [0.5]*len(votes)
+	cons = ({'type':'eq','fun':lambda w: 1-sum(w)})
+	bounds = [(0,1)]*len(votes)
+	res = minimize(log_loss_func, starting_values, 
+		method='SLSQP', bounds=bounds, constraints=cons)
+	print('Ensamble Score: {best_score}'.format(best_score=res['fun']))
+	print('Best Weights: {weights}'.format(weights=res['x']))
+	return res['x']
 
 def shuffle_split(names,datasets,test_size=0.25):
 	selector_i=custom_split(names, test_size=test_size)
@@ -32,8 +54,15 @@ def custom_split(names, test_size=0.25):
 	split=names[:split_size]
 	return SetSelector(split)
 
+def to_one_hot(y,n_cats):
+	one_hot=[]
+	for y_i in y:
+		one_hot.append(np.zeros((n_cats,)))
+		one_hot[-1][y_i]=1.0
+	return np.array(one_hot)
+
 dataset="3DHOI"
 dir_path="../../ICSS"#%s" % dataset
 paths=exp.basic_paths(dataset,dir_path,"dtw","ens/feats")
 paths["common"].append("%s/%s/1D_CNN/feats" % (dir_path,dataset))
-diff_voting(paths["common"],paths["binary"],clf="LR")
+split_voting(paths["common"],paths["binary"],clf="LR")
