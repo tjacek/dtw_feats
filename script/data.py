eba4d406c7d1ea602f3d0cbb4b8f7400c64b3b8a
tjacek <tjacek@agh.edu.pl>
2021-05-14 18:06:12 +0200

    data script

---
+++ script/data.py
@@ -0,0 +1,25 @@
+import sys
+sys.path.append("..")
+import numpy as np
+import exp,ens,files,learn
+
+def save_dataset(common,binary,out_path):
+	datasets=ens.read_dataset(common,binary)
+	files.make_dir(out_path)
+	subdirs={desc_i:"%s/%s" % (out_path,desc_i) 
+			for desc_i in ["features","votes"]}
+	for path_i in subdirs.values():
+		files.make_dir(path_i)
+	for i,data_i in enumerate(datasets):
+		feat_i="%s/%d" % (subdirs["features"],i)
+		data_i.save(feat_i)
+	results=learn.train_ens(datasets,clf="LR")
+	for i,result_i in enumerate( results):
+		votes_i="%s/%d" % (subdirs["votes"],i)
+		np.savetxt(votes_i,result_i.y_pred,delimiter=",")
+
+dataset="3DHOI"
+dir_path="../../ICSS"#%s" % dataset
+paths=exp.basic_paths(dataset,dir_path,"dtw","ens/feats")
+paths["common"].append("%s/%s/1D_CNN/feats" % (dir_path,dataset))
+save_dataset(paths["common"],paths["binary"],'3DHOI_data')
