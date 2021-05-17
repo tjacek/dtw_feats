7918d087f748431f3deaaebec01745915f726786
tjacek <tjacek@agh.edu.pl>
2021-05-17 23:55:28 +0200

    full train

--- hist.py
+++ hist.py
@@ -6,15 +6,31 @@ import files,ens,exp,learn
 
 def acc_hist(common_path,binary_path,clf="LR",
 			out_path=None,title='3DHOI'):
-	datasets=ens.read_dataset(common_path,binary_path)
-	results=learn.train_ens(datasets,clf="LR")
-	votes=ens.Votes(results)
+	votes=full_train(common_path,binary_path,clf)
 	files.make_dir(out_path)
 	for i,result_i in enumerate(votes.results):
 		hist_i=result_i.indv_acc()
 		title_i="%s_%d" % (title,i)
 		plot_i=show_histogram(hist_i,title=title_i,cumsum=False,show=False)
 		plot_i.savefig("%s/%d" % (out_path,i))
+
+def base_train(common,binary,clf):
+	datasets=ens.read_dataset(common,binary)
+	results=learn.train_ens(datasets,clf=clf)
+	return ens.Votes(results)
+
+def full_train(common,binary,clf):
+	datasets=ens.read_dataset(common,binary)
+	results=[]
+	for data_i in datasets:
+		train,test=data_i.split()
+		model=learn.make_model(train,clf)
+		X_train,y_true=train.get_X(),train.get_labels()
+		y_pred=model.predict_proba(X_train)
+		result_i=learn.Result(y_true,y_pred,test.names())
+		results.append(result_i)
+	return ens.Votes(results)
+
 
 def show_histogram(hist,title='hist',cumsum=True,show=True):
 	if(type(hist)==list):
