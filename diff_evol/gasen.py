c44109949adc6b0e3a49c38b17e71e450a8f9022
tjacek <tjacek@agh.edu.pl>
2021-05-26 23:19:39 +0200

    loss function

--- diff_evol/gasen.py
+++ diff_evol/gasen.py
@@ -1,11 +1,41 @@
 import sys
 sys.path.append("..")
 import numpy as np
+from scipy.optimize import differential_evolution
 import ens,exp,files,learn,k_fold
 
-def diff_voting(common,deep,clf="LR"):#,loss="mse"):
+class LossFunc(object):
+    def __init__(self,all_votes):
+        self.all_votes=all_votes
+        self.d=[ result_i.true_one_hot() 
+                  for result_i in all_votes]
+        self.n_iters=0
+
+    def __call__(self,weights):
+        C=self.corl()
+        n_clf=len(self.all_votes)
+        loss=0
+        for i in range(n_clf):
+            for j in range(n_clf):
+                loss+=weights[i]*weights[j] * C[i,j] 
+        self.n_iters+=1
+        print(self.n_iters)
+        print(loss)
+        return loss
+
+    def corl(self):
+        n_clf=len(self.all_votes)
+        C=np.zeros((n_clf,n_clf))
+        for i in range(n_clf):
+            for j in range(n_clf):
+                f_i=self.all_votes[i].y_pred
+                f_j=self.all_votes[j].y_pred
+                c_ij= (f_i-self.d)*(f_j-self.d)
+                C[i,j]=np.mean(c_ij)
+        return C
+
+def diff_voting(common,deep,clf="LR"):
     datasets=ens.read_dataset(common,deep)
-#	weights=np.ones((len(datasets),))
     weights=find_weights(datasets)#,loss=loss)
     results=learn.train_ens(datasets,clf)
     votes=ens.Votes(results)
@@ -14,7 +44,12 @@ def diff_voting(common,deep,clf="LR"):#,loss="mse"):
 
 def find_weights(datasets,clf="LR"):
     results=validation_votes(datasets,clf)
-    raise Exception(len(results))
+    loss_fun=LossFunc(results)
+    bound_w = [(0.0, 1.0)  for _ in datasets]
+    result = differential_evolution(loss_fun, bound_w, 
+    			maxiter=10, tol=1e-7)
+    weights=result['x']
+    return weights
 
 def validation_votes(datasets,clf="LR"):
     train=[data_i.split()[0] for data_i in datasets]
