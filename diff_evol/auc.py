1d25c92c59092261bbee4c7147cee7683bcb815a
tjacek <tjacek@agh.edu.pl>
2021-05-31 23:38:01 +0200

    auc models

--- diff_evol/auc.py
+++ diff_evol/auc.py
@@ -1,14 +1,28 @@
 import sys
 sys.path.append("..")
-import exp,ens#learn
+import numpy as np
+import exp,ens,feats,learn
 
 def auc_exp(common,binary,clf="LR"):
     datasets=ens.read_dataset(common,binary)
     dataset=[date_i.split() for date_i in datasets]
     dataset=list(zip(*dataset))
     train,test=dataset[0],dataset[1]
-    print(len(dataset))
+    names=subsample(train[0],p=0.9)
+    models=[]
+    for train_i in train:
+        new_train_i={name_j:train_i[name_j]
+            for name_j in names}
+        new_train_i=feats.Feats(new_train_i)
+        models.append(learn.make_model(new_train_i,clf))
 #    print(dataset[0].keys())
+
+def subsample(train,p=0.1):
+    all_names=list(train.keys())
+    return [name_i 
+                for name_i in all_names
+                    if(np.random.uniform()<p)]
+
 
 dataset="3DHOI"
 dir_path="../../ICSS"#%s" % dataset
