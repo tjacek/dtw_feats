6c06f2042f1b1fd37ab51014df0a2fedbb7a3bf5
tjacek <tjacek@agh.edu.pl>
2021-05-31 21:37:10 +0200

    add labels

--- script/cf_matrix.py
+++ script/cf_matrix.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sn
 import matplotlib.pyplot as plt
 
-def show_all(in_path,out_path):
+def show_all(in_path,out_path,labels=None):
     if(not os.path.isdir(out_path)):
         os.mkdir(out_path)
     if(not os.path.isdir(in_path)):
@@ -12,11 +12,13 @@ def show_all(in_path,out_path):
         show_confusion(in_path,title=title,out_path=out_path,
             colors="grey")
     all_paths=os.listdir(in_path)
-    for path_i in all_paths:
-        in_i=in_path+'/'+path_i
-        out_i=out_path+'/'+path_i
+    for i,path_i in enumerate(all_paths):
+        in_i="%s/%s"  % (in_path,path_i)
+        out_i="%s/%s" % (out_path,path_i)
         print(out_i)
-        show_confusion(in_path=in_i,title=path_i,out_path=out_i)
+        labels_i=labels[i] if(labels) else None
+        show_confusion(in_path=in_i,title=path_i,out_path=out_i,
+            labels=labels_i,colors="grey")
 
 def show_confusion(in_path,labels=None,title=None,out_path=None,
                     colors="base"):       	
@@ -28,7 +30,6 @@ def show_confusion(in_path,labels=None,title=None,out_path=None,
         labels=range(dim[0])
     df_cm = pd.DataFrame(cf_matrix, labels,labels)
 #    sn.set(font_scale=1.0)
-
     sn.heatmap(df_cm,cmap=cmap_dict[colors],#linewidths=0.5,
     	annot=True,annot_kws={"size": 9}, fmt='g')
     if(title):
@@ -48,17 +49,21 @@ def show_confusion(in_path,labels=None,title=None,out_path=None,
         plt.show()
 
 
-cats_msr=['high arm wave','horizontal arm wave','hammer','hand catch','forward punch',
+cats_MSR=['high arm wave','horizontal arm wave','hammer','hand catch','forward punch',
           'high throw','draw x','draw tick','draw circle','hand clap',
           'two hand wave','side-boxing','bend','forward kick','side kick',
           'jogging','tennis swing','tennis serve','golfswing','pick up & throw']
 
-cats_mhad=['right arm swipe to the left', 'right arm swipe to the right', 'right hand wave','two hand front clap','right arm throw',
+cats_MHAD=['right arm swipe to the left', 'right arm swipe to the right', 'right hand wave','two hand front clap','right arm throw',
  'cross arms in the chest','basketball shoot','right hand draw x', 'draw circle (clockwise)','draw circle (counter clockwise)', 
  'draw triangle','bowling', 'front boxing', 'baseball swing from right', 'tennis right hand forehand swing','arm curl', 
  'tennis serve', 'two hand push', 'right hand knock on door', 'right hand catch an object', 'right hand pick up and throw', 'jogging in place', 
  'walking in place', 'sit to stand', 'stand to sit','forward lunge','squat']
 
+cats_3DHOI=['drinking','pouring','calling phone','playing phone',
+'wearing backpacks','packing backpacks','sitting chair','moving chair',
+'taking out wallet','taking from wallet','mopping','sweeping']
+
 in_path="../../ICSS/wyniki/confusion_matrix/raw/"
 #show_confusion("cf/pairs",None,"pairs")
-show_all(in_path,"plots")
+show_all(in_path,"plots",labels=[cats_MHAD,cats_3DHOI])
