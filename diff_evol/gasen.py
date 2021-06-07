771389b619a7c08da880a5c1059844ea65d5a037
tjacek <tjacek@agh.edu.pl>
2021-06-07 20:05:16 +0200

    refactore gasen

--- diff_evol/gasen.py
+++ diff_evol/gasen.py
@@ -2,16 +2,14 @@ import sys
 sys.path.append("..")
 import numpy as np
 from scipy.optimize import differential_evolution
-import ens,exp,files,learn,k_fold
+import exp,ens,k_fold,learn
 
-class LossFunc(object):
-    def __init__(self,all_votes,acc=True):
-        self.all_votes=ens.Votes(all_votes)
+class Corl(object):
+    def __init__(self,all_votes):
+        self.all_votes=ens.Votes(all_votes)	
         self.d=[ result_i.true_one_hot() 
                   for result_i in all_votes]
-        self.n_iters=0
-        self.acc=acc
-
+    
     def __call__(self,weights):
         norm=weights/np.sum(weights)
         C=self.corl()
@@ -19,13 +17,7 @@ class LossFunc(object):
         loss=0
         for i in range(n_clf):
             for j in range(n_clf):
-                loss+=weights[i]*weights[j] * C[i,j] 
-        if(self.acc):
-            result=self.all_votes.weighted(norm)
-            loss+= (1.0-result.get_acc())
-        self.n_iters+=1
-        print(self.n_iters)
-        print(loss)
+                loss+=weights[i]*weights[j] * C[i,j] 	
         return loss
 
     def corl(self):
@@ -40,35 +32,41 @@ class LossFunc(object):
                 C[i,j]=np.mean(c_ij)
         return C
 
-def diff_voting(common,deep,clf="LR"):
-    datasets=ens.read_dataset(common,deep)
-    weights=find_weights(datasets)#,loss=loss)
-    results=learn.train_ens(datasets,clf)
-    votes=ens.Votes(results)
-    result=votes.weighted(weights)
-    return result
+class OptimWeights(object):
+    def __init__(self, loss=None):
+        if(loss is None):
+            loss=Corl
+        self.loss = loss
 
-def find_weights(datasets,clf="LR"):
-    results=validation_votes(datasets,clf)
-    loss_fun=LossFunc(results)
-    bound_w = [(0.01, 1.0)  for _ in datasets]
-    result = differential_evolution(loss_fun, bound_w, 
+    def __call__(self,common,deep,clf="LR"):
+        datasets=ens.read_dataset(common,deep)
+        weights=self.find_weights(datasets)
+        results=learn.train_ens(datasets,clf)
+        votes=ens.Votes(results)
+        result=votes.weighted(weights)
+        return result
+
+    def find_weights(self,datasets,clf="LR"):
+        results=self.validation_votes(datasets,clf)
+        loss_fun=self.loss(results)
+        bound_w = [(0.01, 1.0)  for _ in datasets]
+        result = differential_evolution(loss_fun, bound_w, 
     			maxiter=10, tol=1e-7)
-    weights=result['x']
-    return weights
+        weights=result['x']
+        return weights
 
-def validation_votes(datasets,clf="LR"):
-    train=[data_i.split()[0] for data_i in datasets]
-    names=list(train[0].keys())
-    selector_gen=k_fold.StratGen(1)
-    selector=list(selector_gen(names))[0]
-    return learn.train_ens(train,clf=clf,selector=selector)
-
+    def validation_votes(self,datasets,clf="LR"):
+        train=[data_i.split()[0] for data_i in datasets]
+        names=list(train[0].keys())
+        selector_gen=k_fold.StratGen(1)
+        selector=list(selector_gen(names))[0]
+        return learn.train_ens(train,clf=clf,selector=selector)
 
 dataset="3DHOI"
 dir_path="../../ICSS"#%s" % dataset
 paths=exp.basic_paths(dataset,dir_path,"dtw","ens/feats")
 paths["common"].append("%s/%s/1D_CNN/feats" % (dir_path,dataset))
-
-result=diff_voting(paths["common"],paths["binary"],clf="LR")
+print(paths)
+optim=OptimWeights(Corl)
+result=optim(paths["common"],paths["binary"])
 result.report()
