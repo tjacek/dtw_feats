ac6784e8908cb2b15b26d766d7208ba45b132e42
tjacek <tjacek@agh.edu.pl>
2021-06-18 13:06:08 +0200

    count cats

--- script/3DHOI.py
+++ script/3DHOI.py
@@ -1,10 +1,25 @@
 import re,os,shutil
+import numpy as np
 
-def format_names(in_path,out_path=None):
+def count_cats(in_path,out_path=None):
+    paths=get_paths(in_path)
+    paths=[get_paths(get_paths(path_i)[0]) for path_i in paths]
+    cats=np.zeros((12,))
+    for person_i in paths:
+        for path_i in  person_i:	
+            cats[get_cat(path_i)-1]+=1
+    print(cats)
+
+def get_paths(in_path):
     paths=os.listdir(in_path)
-    persons=list(set(paths))
-    print(persons)
+    return ["%s/%s" % (in_path,path_i)  
+                for path_i in paths]
+
+def get_cat(path_i):
+    name_i=path_i.split("/")[-1]
+    return int(re.findall(r'\d+', name_i)[0])
+
 
 path="../../raw_3DHOI/SYSU"
 
-format_names(path)
+count_cats(path)
